5d4d0707f025fd80d2c01774f19b37d6b662a74e
RuslanPr0g <theruslan.prog@gmail.com>
2025-10-07 19:09:56 +0200

    do not run jobs in master

--- server/src/Hiscary.Recommendations.Jobs/DIModule.cs
+++ server/src/Hiscary.Recommendations.Jobs/DIModule.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using StackNucleus.DDD.Jobs.Quartz.Extensions;
 
 namespace Hiscary.Recommendations.Jobs;
 
@@ -7,14 +6,15 @@ public static class DIModule
 {
     public static IServiceCollection AddJobs(this IServiceCollection services)
     {
-        return services.AddConfigurableJobs([
-            new JobConfiguration
-            {
-                Key = nameof(IndexStoriesJob),
-                Type = typeof(IndexStoriesJob),
-                RepeatInterval = 69,
-                RepeatForever = true
-            }
-        ]);
+        //return services.AddConfigurableJobs([
+        //    new JobConfiguration
+        //    {
+        //        Key = nameof(IndexStoriesJob),
+        //        Type = typeof(IndexStoriesJob),
+        //        RepeatInterval = 69,
+        //        RepeatForever = true
+        //    }
+        //]);
+        return services;
     }
 }
