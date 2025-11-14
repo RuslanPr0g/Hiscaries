265d8b95db88a2f79a768da2811c903016253548
RuslanPr0g <theruslan.prog@gmail.com>
2025-11-14 13:55:16 +0100

    Improve reset git script

--- scripts/reset-to-master.sh
+++ scripts/reset-to-master.sh
@@ -2,8 +2,9 @@
 set -e
 
 git checkout master -f
-git fetch origin --prune
 git pull origin master
+
+git fetch origin --prune
 
 for b in $(git branch --format="%(refname:short)"); do
     if [[ "$b" != "master" ]]; then
