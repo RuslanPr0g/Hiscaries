2b130d276bf97404c3a31835baec070af035f587
RuslanPr0g <theruslan.prog@gmail.com>
2025-11-14 16:40:12 +0100

    Revert "Client > Fix route rewrite http-https because of the Aspire default behavior"
    This reverts commit a0cfec735ebc3d5feac5235ac4dcd6c6cf69fe4c.

--- server/src/Hiscary.AppHost/Program.cs
+++ server/src/Hiscary.AppHost/Program.cs
@@ -82,7 +82,7 @@ var media = builder.AddProject<Projects.Hiscary_Media_Api_Rest>("hc-media-api-re
     .WaitFor(redis)
     .WithJwtAndSaltSettings(builder.Configuration)
     .WithHttpsEndpoint(name: "rest", port: 7014, targetPort: 7014, isProxied: false)
-    .WithEnvironment("ServiceUrls__MediaServiceUrl", "https://localhost:5000/api/v1/media")
+    .WithEnvironment("ServiceUrls__MediaServiceUrl", "https://localhost:5001/api/v1/media")
     .WithReference(rabbitmq)
     .WithReference(azBlobs);
 var recommendations = builder.AddProject<Projects.Hiscary_Recommendations_Api_Rest>("hc-recommendations-api-rest")
@@ -99,7 +99,7 @@ var recommendations = builder.AddProject<Projects.Hiscary_Recommendations_Api_Re
     .WithReference(azBlobs);
 
 var gateway = builder.AddYarp("apigateway")
-                     .WithHostPort(5000)
+                     .WithHostPort(5001)
                      .WaitFor(useraccounts)
                      .WaitFor(notifications)
                      .WaitFor(platformusers)
