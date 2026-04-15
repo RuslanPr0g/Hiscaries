3510e7dd5261bd23457e9df6d7db5281d3419ffc
RuslanPr0g <theruslan.prog@gmail.com>
2026-04-15 16:01:38 +0200

    chore: bug fix for MarkAnnotatedPdfConflict

--- server/src/Hiscary.Stories.EventHandlers/IntegrationEvents/DocumentConsolidatedIntegrationEventHandler.cs
+++ server/src/Hiscary.Stories.EventHandlers/IntegrationEvents/DocumentConsolidatedIntegrationEventHandler.cs
@@ -1,17 +1,21 @@
 using Hiscary.Media.IntegrationEvents.Outgoing;
 using Hiscary.Shared.Domain.ValueObjects;
 using Hiscary.Stories.Domain.DataAccess;
+using Hiscary.Stories.IntegrationEvents.Outgoing;
 using Microsoft.Extensions.Logging;
 using StackNucleus.DDD.Domain.EventHandlers;
+using StackNucleus.DDD.Domain.EventPublishers;
 using Wolverine;
 
 namespace Hiscary.Stories.EventHandlers.IntegrationEvents;
 
 public sealed class DocumentConsolidatedIntegrationEventHandler(
+    IEventPublisher publisher,
     IStoryWriteRepository repository,
     ILogger<DocumentConsolidatedIntegrationEventHandler> logger)
     : IEventHandler<DocumentConsolidatedIntegrationEvent>
 {
+    private readonly IEventPublisher _publisher = publisher;
     private readonly IStoryWriteRepository _repository = repository;
     private readonly ILogger<DocumentConsolidatedIntegrationEventHandler> _logger = logger;
 
@@ -35,6 +39,8 @@ public sealed class DocumentConsolidatedIntegrationEventHandler(
         story.SetExternalPdf(externalPdf, pageCount);
 
         await _repository.SaveChanges();
+
+        await _publisher.Publish(new StoryPdfUpdatedIntegrationEvent(storyId, integrationEvent.PdfUrl));
 
         _logger.LogInformation("External PDF consolidated for story {StoryId}", storyId);
     }
